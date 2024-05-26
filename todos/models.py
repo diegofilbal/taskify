57cc3529b0f5422f07bb6c5864db0106075b06a4
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-26 20:17:23 -0300

    feat: add todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True)
