143d02bc3f813ea418c04da662d277fae692452d
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-27 01:33:23 -0300

    refactor: change create list page implementation from fbv to cbv

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Todo
 
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+class TodoListView(ListView):
+    model = Todo
