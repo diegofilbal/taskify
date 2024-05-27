a8c097515f6d02bd13c54d71ba6131a27c737c8c
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-27 03:35:41 -0300

    feat: change list ordering

--- todos/models.py
+++ todos/models.py
@@ -6,3 +6,6 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateField(null=False, blank=False)
     finished_at = models.DateField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
