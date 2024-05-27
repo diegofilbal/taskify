5ebe1af207ed8cfd85e5ea5d76541f8d1c2177e0
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-27 02:05:36 -0300

    feat: implement task creation page

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Todo
+from django.urls import reverse_lazy
 
 
 class TodoListView(ListView):
     model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
